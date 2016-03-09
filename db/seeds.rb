6377c18795e273f370b2d490b1b990b789f75b45
samuel barnabas <sbarnabas@norauto.com>
2016-03-09 15:32:41 +0100

    MAJ seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Lift.create(name: 'Garage Vroom', address: '13 rue de la Gaité 59100 Roubaix')
+Lift.create(name: 'Garage de la Rivière', address: '31 rue de la Fonderie 59200 Tourcoing' )
+Lift.create(name: 'Speedy up', address: '43 rue de la Blanche Porte 59200 Tourcoing')
+Lift.create(name: 'Jacky Mécano', address: '3 rue des Champs 59223 Roncq')
+Lift.create(name: 'Ma Belle Auto', address: '143 avenue de la Liberté 59000 Lille')
+
+Owner.create(name: 'George', address: '13 rue de la Gaité 59100 Roubaix' , phone: '03 20 12 45 45')
+Owner.create(name: 'Abdel', address: '31 rue de la Fonderie 59200 Tourcoing' , phone: '06 32 21 54 14')
+Owner.create(name: 'Thomas', address: '43 rue de la Blanche Porte 59200 Tourcoing', phone: '06 17 17 17 18')
+Owner.create(name: 'Floflo', address: '3 rue des Champs 59223 Roncq', phone: '06 43 43 43 48')
+Owner.create(name: 'Samy', address: '143 avenue de la Liberté 59000 Lille', phone: '03 20 31 63 98')
+
